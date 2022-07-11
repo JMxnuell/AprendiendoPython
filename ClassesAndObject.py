8c35a5319fbdc3fb0f08a53ff580fbdba2f63cee
Manuel <jsuarez1804@alumno.ipn.mx>
2022-07-11 15:37:34 -0500

    function object and inheritance

--- ClassesAndObject.py
+++ ClassesAndObject.py
@@ -1,8 +1,18 @@
 
-class Student:
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Student(Person):
     def __init__(self, name, age, major):
         self.name = name
         self.age = age
         self.major = major
+
+    def view(self):
+        print(self.name)
+
+
 
 
--- studentObect.py
+++ studentObect.py
@@ -2,4 +2,4 @@ from ClassesAndObject import  Student
 
 student1 = Student("Manuel",22,"ING")
 
-print(student1.name)
+print(student1.view())
